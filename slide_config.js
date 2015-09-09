c6154e0fcdcafe5c53fcce8e4066a42171768bd9
Morgan Estes <morgan.estes@gmail.com>
2015-09-09 14:52:43 -0500

    Move font stylesheets into their own directory

--- slide_config.js
+++ slide_config.js
@@ -9,10 +9,10 @@ var SLIDE_CONFIG = {
     },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
-    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
-    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    enableSlideAreas: false, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: false, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in index.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/wpmini-blue.png',
     webFonts: [
       //'Open Sans:regular,semibold,italic,italicsemibold',
       //'Source Code Pro'
